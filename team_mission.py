3c16d6f7163fef6bc73463ea87a3c195e00b26cc
손진우 <sonjinwoo0729@gmail.com>
2024-07-10 09:50:04 +0900

    여러분들이 올려주신거 보고 전체적으로 수정해서 최종본으로 올립니다. 민석님꺼 대부분 참고했어요.

--- team_mission.py
+++ team_mission.py
@@ -19,52 +19,75 @@ class Post:
             f"Title: {self.title}\nContent: {self.content}\nAuthor: {self.author}")
 
 
-member1 = Member("", "", "")
-member2 = Member("", "", "")
-member3 = Member("", "", "")
-
 members = []
-members.append(member1)
-members.append(member2)
-members.append(member3)
+posts = []
+members_id = []
+
+# 터미널에서 유저를 추가하는 기능
+
+
+def plus_user():
+    while True:
+        name = input("이름을 입력해주세요: ").strip()
+        username = input("ID를 입력해주세요: ").strip()
+        if username in members_id:
+            print("해당 ID는 사용중입니다. 다른 ID를 사용해주세요.")
+            continue
+        password = input("패스워드를 입력해주세요: ").strip()
+
+        members.append(Member(name, username, password))
+        members_id.append(username)
+
+        if input("회원을 계속 추가하시겠습니까? (y/n): ").lower() == "y":
+            print()
+            continue
+        else:
+            break
+    print()
+
+# 터미널에서 게시글을 입력하는 기능
+
+
+def plus_post():
+    while True:
+        title = input("게시글 제목을 입력해주세요: ").strip()
+        content = input("게시글 내용을 입력해주세요: ").strip()
+        author = input("게시글 작성자를 입력해주세요: ").strip()
+
+        if author in members_username:
+            posts.append(post(title, content, author))
+        else:
+            print("해당 ID를 가진 회원이 존재하지 않습니다. ID를 다시 확인해주세요.")
+            continue
+
+        if input("게시물을 계속 추가하시겠습니까? (y/n): ").lower() == "y":
+            print()
+            continue
+        else:
+            break
+    print()
+
 
 # 회원 이름 출력
 print("All Members:")
 for member in members:
     print(member.name)
 
-# 게시글 작성
-post1 = Post("", member1.username)
-post2 = Post("", member1.username)
-post3 = Post("", member1.username)
 
-post4 = Post("", member2.username)
-post5 = Post("", member2.username)
-post6 = Post("",
-             "", member2.username)
-
-post7 = Post("",
-             "", member3.username)
-post8 = Post("", "",
-             member3.username)
-post9 = Post("",
-             "", member3.username)
-
-# posts 리스트에 게시물 추가
-posts = []
-posts.extend([post1, post2, post3, post4, post5, post6, post7, post8, post9])
-
-# 각 회원이 작성한 게시글 제목 출력
-print("\nPosts by each Member:")
-for member in members:
-    print(f"{member.name}'s Posts:")
+# 회원이 작성한 게시글 제목 출력
+def find_post():
+    user_name = input('누구의 게시글을 찾을까요?')
+    print(f"{user_name}'s Posts:")
     for post in posts:
-        if post.author == member.username:
+        if post.author == user_name:
             print(post.title)
 
 # 특정 단어가 포함된 게시글 제목 출력
-keyword = ""
-print(f"\nPosts containing '{keyword}' in content:")
-for post in posts:
-    if keyword in post.content:
-        print(post.title)
+
+
+def find_post_key():
+    keyword = input("어떤 키워드를 검색하시겠습니까?")
+    print(f"\nPosts containing '{keyword}' in content:")
+    for post in posts:
+        if keyword in post.content:
+            print(post.title)
